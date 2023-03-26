7417d69381a419341f3c4bb3306a4d74221719f5
YugmaGandhi <yugmagandhi1805@gmail.com>
2023-03-26 23:48:35 +0530

    added put request for reminder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,6 +26,12 @@ const fetchTasks = async ()=> {
   return data;
 };
 
+const fetchTask = async (id)=> {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`);
+  const data = await res.json();
+
+  return data;
+};
 
   //Add Task
   const addTask = async (task) => {
@@ -59,7 +65,20 @@ const fetchTasks = async ()=> {
     setFormVal(!formVal)
   }
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder};
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'PUT',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
+
+    const data = await res.json();
+
     setTask(tasks.map((task)=>{
       if(task.id === id){
         return {...task, reminder:!task.reminder};
